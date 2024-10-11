4ebf4e8b6025e30c17257b7d8709afef36e30fa3
UNYUZIMFURA Kevin <banjoflylive@gmail.com>
2024-10-11 20:19:07 +0200

    Feat: Counting Likes"

--- controllers/likes.controller.js
+++ controllers/likes.controller.js
@@ -3,13 +3,13 @@ const prisma = new PrismaClient();
 
 const likePost = async (req, res) => {
   const userId = req.user.id;
-  const postId  = req.params.id;
+  const postId = req.params.id;
 
-  if(!postId) {
+  if (!postId) {
     return res.status(400).json({
-        success: false,
-        message: "Provide a Post ID!"
-    })
+      success: false,
+      message: "Provide a Post ID!",
+    });
   }
 
   try {
@@ -19,24 +19,31 @@ const likePost = async (req, res) => {
         postId,
       },
     });
-    if(isLikeExisting) {
-        return res.status(400).json({
-            success: false,
-            message: "Already liked the post"
-        })
+
+    if (isLikeExisting) {
+      return res.status(400).json({
+        success: false,
+        message: "Already liked the post",
+      });
     }
+
     const like = await prisma.like.create({
-        data: {
-            userId,
-            postId
-        }
-    })
+      data: {
+        userId,
+        postId,
+      },
+    });
+
+    await prisma.post.update({
+      where: { id: postId },
+      data: { likesCount: { increment: 1 } },
+    });
 
     return res.status(200).json({
-        success: true,
-        message: "Successfully liked the post",
-        like
-    })
+      success: true,
+      message: "Successfully liked the post",
+      like,
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
@@ -46,34 +53,41 @@ const likePost = async (req, res) => {
   }
 };
 
-const unlikePost = async(req,res) => {
-    const userId = req.user.id
-    const {postId} = req.body
-    try {
-      const like = await prisma.like.findFirst({
-        where: {
-            userId,
-            postId
-        }
-      })
-      if(!like) {
-        return res.status(400).json({
-            success: false,
-            message: "There is no like associated with the provided post"
-        })
-      }
-      await prisma.like.delete({
-        where: {
-            id: like.id
-        }
-      })
-      return res.status(204).json({})
-    } catch(err) {
-       return res.status(500).json({
+const unlikePost = async (req, res) => {
+  const userId = req.user.id;
+  const { postId } = req.body;
+  try {
+    const like = await prisma.like.findFirst({
+      where: {
+        userId,
+        postId,
+      },
+    });
+
+    if (!like) {
+      return res.status(400).json({
         success: false,
-        message: "Error unliking post"
-       })
+        message: "There is no like associated with the provided post",
+      });
     }
- }
+
+    await prisma.like.delete({
+      where: {
+        id: like.id,
+      },
+    });
+
+    await prisma.post.update({
+        where: { id: postId },
+        data: { likesCount: { decrement: 1 } }
+      });
+    return res.status(204).json({});
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Error unliking post",
+    });
+  }
+};
 
 module.exports = { likePost, unlikePost };
